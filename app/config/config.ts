a53b60fd6723db552a7147503014502c961b3bfd
serverConnected <sarvadshetty@gmail.com>
2025-02-13 00:08:25 +0530

    initial setup

---
+++ app/config/config.ts
@@ -0,0 +1,16 @@
+import { http } from "wagmi";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { baseSepolia } from "wagmi/chains";
+// import { coinbaseWallet } from "wagmi/connectors";
+
+export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID;
+if (!projectId) throw new Error("Project ID is not defined");
+
+export const config = getDefaultConfig({
+  appName: "UBI",
+  projectId: projectId,
+  chains: [baseSepolia],
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
