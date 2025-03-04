4be93612e7f439c6e27daf659adc4d2072da9cb6
serverConnected <sarvadshetty@gmail.com>
2025-03-04 21:16:33 +0300

    updated assets for framesv2

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,22 +16,21 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// export const metadata: Metadata = {
-//   title: "UBI",
-//   description: "Created by @serverConnectd",
-// };
-
 const frameMetadata = {
   version: "next",
-  imageUrl: `${String(process.env.NEXT_PUBLIC_HOST_URL)}/splash.png`,
+  imageUrl: `${String(
+    process.env.NEXT_PUBLIC_HOST_URL
+  )}/BUILD-previewFrame.png`,
   button: {
-    title: "UBI",
+    title: "Launch App",
     action: {
       type: "launch_frame",
       name: "UBI Mini App",
       url: `${String(process.env.NEXT_PUBLIC_HOST_URL)}/`,
-      splashImageUrl: `${String(process.env.NEXT_PUBLIC_HOST_URL)}/splash.png`,
-      splashBackgroundColor: "#000000",
+      splashImageUrl: `${String(
+        process.env.NEXT_PUBLIC_HOST_URL
+      )}/BUILDLogo.png`,
+      splashBackgroundColor: "#0000EB",
     },
   },
 };
